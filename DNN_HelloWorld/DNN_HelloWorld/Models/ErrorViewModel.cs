757d8a8736033b83731bb7f30a885c35bbf8c6c4
rigozsolt <zsolt.rigo@datatronic.hu>
2024-04-09 16:03:03 +0200

    Added working initial commit

---
+++ DNN_HelloWorld/DNN_HelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DNN_HelloWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
